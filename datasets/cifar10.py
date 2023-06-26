3c6bce7b73b38e097e3c0e0722b76bdde00b3688
Swapnil Gusani <swapniel99@gmail.com>
2023-06-26 21:09:41 +0530

    albumentations bugfixes

--- datasets/cifar10.py
+++ datasets/cifar10.py
@@ -1,28 +1,41 @@
+import numpy as np
 import torch
 from torchvision import datasets
 import albumentations as A
 
-from .generic import DataSet
+from .generic import MyDataSet
 
 
-class CIFAR10(DataSet):
+class AlbCIFAR10(datasets.CIFAR10):
+    def __init__(self, root, alb_transform=None, **kwargs):
+        super(AlbCIFAR10, self).__init__(root, **kwargs)
+        self.alb_transform = alb_transform
+
+    def __getitem__(self, index):
+        image, label = super(AlbCIFAR10, self).__getitem__(index)
+        if self.alb_transform is not None:
+            image = self.alb_transform(image=np.array(image))['image']
+        return image, label
+
+
+class CIFAR10(MyDataSet):
     mean = (0.49139968, 0.48215827, 0.44653124)
     std = (0.24703233, 0.24348505, 0.26158768)
     classes = None
 
     def get_train_transforms(self):
-        if self.augment_transforms is None:
-            self.augment_transforms = [
-                A.HorizontalFlip(),
-                A.ToGray(),
-                A.Rotate(limit=7, p=1.)
+        if self.alb_transforms is None:
+            self.alb_transforms = [
+                A.HorizontalFlip(p=0.5),
+                A.ToGray(p=0.1),
+                A.Rotate(limit=15, p=1.)
             ]
         return super(CIFAR10, self).get_train_transforms()
 
     def get_train_loader(self):
         super(CIFAR10, self).get_train_loader()
 
-        train_data = datasets.CIFAR10('../data', train=True, download=True, transform=self.train_transforms)
+        train_data = AlbCIFAR10('../data', train=True, download=True, alb_transform=self.train_transforms)
         if self.classes is None:
             self.classes = {i: c for i, c in enumerate(train_data.classes)}
         self.train_loader = torch.utils.data.DataLoader(train_data, shuffle=self.shuffle, **self.loader_kwargs)
@@ -31,7 +44,7 @@ class CIFAR10(DataSet):
     def get_test_loader(self):
         super(CIFAR10, self).get_test_loader()
 
-        test_data = datasets.CIFAR10('../data', train=False, download=True, transform=self.test_transforms)
+        test_data = AlbCIFAR10('../data', train=False, download=True, alb_transform=self.test_transforms)
         self.test_loader = torch.utils.data.DataLoader(test_data, shuffle=False, **self.loader_kwargs)
         return self.test_loader
 
--- datasets/generic.py
+++ datasets/generic.py
@@ -1,50 +1,47 @@
 import os
-import numpy as np
+from abc import abstractmethod
 from matplotlib import pyplot as plt
 import albumentations as A
-from albumentations.pytorch import ToTensorV2 as ToTensor
-from abc import abstractmethod
+from albumentations.pytorch import ToTensorV2
 
 
-class DataSet(object):
+class MyDataSet(object):
     mean = None
     std = None
     classes = None
 
-    def __init__(self, batch_size=32, augment_transforms=None, shuffle=True):
+    def __init__(self, batch_size=32, alb_transforms=None, shuffle=True):
         self.batch_size = batch_size
-        self.augment_transforms = augment_transforms
+        self.alb_transforms = alb_transforms
         self.shuffle = shuffle
         self.loader_kwargs = {'batch_size': batch_size, 'num_workers': os.cpu_count()}
         self.std_transforms = [
-            ToTensor(),
-            A.Normalize(self.mean, self.std)
+            A.Normalize(self.mean, self.std),
+            ToTensorV2()
         ]
-        self.train_transforms = None
-        self.test_transforms = None
-        self.train_loader, self.test_loader = self.get_loaders()
+        self.train_transforms = self.get_train_transforms()
+        self.test_transforms = self.get_test_transforms()
+        self.train_loader = self.get_train_loader()
+        self.test_loader = self.get_test_loader()
         self.example_iter = iter(self.train_loader)
 
     def get_train_transforms(self):
         all_transforms = list()
-        if self.augment_transforms is not None:
-            all_transforms += self.augment_transforms
+        if self.alb_transforms is not None:
+            all_transforms += self.alb_transforms
         all_transforms += self.std_transforms
-        return lambda x: A.Compose(all_transforms)(image=np.array(x))['image']
+        return A.Compose(all_transforms)
 
     def get_test_transforms(self):
-        return lambda x: A.Compose(self.std_transforms)(image=np.array(x))['image']
+        return A.Compose(self.std_transforms)
 
     @abstractmethod
     def get_train_loader(self):
-        self.train_transforms = self.get_train_transforms()
+        pass
 
     @abstractmethod
     def get_test_loader(self):
-        self.test_transforms = self.get_test_transforms()
-
-    def get_loaders(self):
-        return self.get_train_loader(), self.get_test_loader()
+        pass
 
     @classmethod
     def denormalise(cls, tensor):
