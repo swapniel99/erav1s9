3976daf5668bed546ea829b7ad5e6485953d6e2d
Swapnil Gusani <swapniel99@gmail.com>
2023-06-30 17:32:29 +0530

    update augmentations

--- datasets/cifar10.py
+++ datasets/cifar10.py
@@ -29,14 +29,14 @@ class CIFAR10(MyDataSet):
             self.alb_transforms = [
                 A.Downscale(0.8, 0.95, p=0.2, interpolation={'downscale': cv2.INTER_AREA,
                                                              'upscale': cv2.INTER_NEAREST}),
-                A.ColorJitter(0.1, 0.1, 0.1, 0.1, p=0.1),
-                A.ToGray(p=0.2),
+                A.ColorJitter(0.1, 0.1, 0.1, 0.1, p=0.2),
+                A.ToGray(p=0.1),
                 A.HorizontalFlip(p=0.5),
                 A.ShiftScaleRotate(shift_limit=0.0625, scale_limit=0.1, rotate_limit=15),
                 # Padding value doesnt matter here.
                 A.PadIfNeeded(64, 64, border_mode=cv2.BORDER_CONSTANT, value=0, p=1),
                 # Since normalisation was the first step, mean is already 0, so cutout fill_value = 0
-                A.CoarseDropout(max_holes=1, max_height=16, max_width=16, fill_value=0, p=1),
+                A.CoarseDropout(max_holes=1, max_height=16, max_width=16, fill_value=0, p=0.6),
                 A.CenterCrop(32, 32, p=1)
             ]
         return super(CIFAR10, self).get_train_transforms()
